ead7f53a828cc86d4e8261ba9548de531f53d6c5
Gabriel Possas <gabrielpossas2014@gmail.com>
2022-10-08 20:48:18 -0700

    login-firebase-complete

--- src/pages/redefinationPass.tsx
+++ src/pages/redefinationPass.tsx
@@ -17,7 +17,7 @@ export default function RecuperationPass() {
 
    function resetPassword(e: FormEvent) {
       e.preventDefault()
-      const code: string = query.oobCOde !== undefined ? query.oobCode! : '';
+      const code: string = query.oobCOde as string;
       console.log(code)
       if (password === passwordConfirmation) {
          try {
