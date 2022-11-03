9705bd3733c09e7cc15867070d773a51830a7549
Gabriel Possas <gabrielpossas2014@gmail.com>
2022-11-02 17:39:58 -0700

    head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,9 @@ export default class MyDocument extends Document {
       return (
          <Html>
             <Head>
-               
+               <title>IDEA - Maker</title>
+               <link rel="shortcut icon" type="image/svg" href="LogoIcon.svg"/>
+
                <link rel="preconnect" href="https://fonts.googleapis.com"/>
                <link rel="preconnect" href="https://fonts.gstatic.com" />
                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
