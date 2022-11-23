a36f2c62169da7f4e7f0089ea2c3bd3eeb9756ce
Gabriel Possas <gabrielpossas2014@gmail.com>
2022-11-23 18:34:51 -0400

    button-add-project-animation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -89,7 +89,7 @@ export default function Home() {
                </div>
 
                <button disabled={(!newProject && addDoc) && true} onClick={() => addDoc ? addProject() : setaddDoc(!addDoc)}
-                  className="bg-gray-100 p-1 rounded-full relative top-0 hover:-top-1 disabled:shadow-gray disabled:cursor-not-allowed disabled:hover:text-gray-500
+                  className="bg-gray-100 p-1 rounded-full relative top-0 hover:-top-1 hover:disabled:shadow-gray disabled:shadow-gray  disabled:cursor-not-allowed disabled:hover:text-gray-500
                   left-0 hover:left-1 duration-500 text-gray-500 hover:text-orange-700 shadow-orangelg hover:shadow-orangeButton"
                >  
                   { addDoc ? (
@@ -99,16 +99,20 @@ export default function Home() {
                   )}
                </button>
                
-               { addDoc && (
-                  <div className={`flex items-center gap-4 duration-500 transition-all delay-200 ${addDoc ? 'opacity-100' : 'opacity-0'}`}>
-                     <input autoFocus className={`p-1 px-3 w-max ml-8 sm:ml-2 max-w-[400px] outline-none bg-gray-50 rounded-lg border-2 border-red-600 border-opacity-60 ${newProject != '' ? 'border-green-700' : ''}`} value={newProject} onChange={(e) => setNewProject(e.target.value)}/>
-                     
-                     <button className="p-1 rounded-full shadow-orangelg hover:shadow-orangeButton text-gray-500 top-0 hover:-top-1
-                     left-0 hover:left-1 duration-500 relative" onClick={() => {setaddDoc(false), setNewProject('')}}> <FiX size={26}/></button>
+               
+                  <div className={` duration-200 transition-all ${addDoc ? 'opacity-100' : 'opacity-0'}`}>
+                     <div className={`flex items-center gap-4 duration-300 transition-all ${addDoc ? 'translate-y-0' : '-translate-y-12'}`}>
+                        <input autoFocus className={`p-1 px-3 w-max ml-8 sm:ml-2 max-w-[400px] outline-none bg-gray-50 rounded-lg border-2 border-red-600 border-opacity-60 ${newProject != '' ? 'border-green-700' : ''}`} value={newProject} onChange={(e) => setNewProject(e.target.value)}/>
+                        
+                        <button className="p-1 rounded-full shadow-orangelg hover:shadow-orangeButton bg-gray-50 text-gray-500 top-0 hover:-top-1
+                        left-0 hover:left-1 duration-500 relative" onClick={() => {setaddDoc(false), setNewProject('')}}> 
+                           <FiX size={26}/>
+                        </button>
+                     </div>
                   </div>
-               )}
+               
             </div>
-            <div className="absolute left-[5%] sm:left-[20%] w-[90%] h-[60%] sm:h-[60%] sm:w-[60%] mt-16 z-10 blur-sm flex justify-center items-center bg-orange-500/30
+            <div className="absolute left-[5%] sm:left-[20%] w-[90%] h-[60%] sm:h-[60%] sm:w-[60%] mt-16 -z-10 blur-sm flex justify-center items-center bg-orange-500/30
                rounded-[30%_70%_42%_58%_/_67%_28%_72%_33%] animate-[bgDistorcao_12s_ease-in-out_infinite]" 
             >
                <img src="BG-Home.svg" className="w-44"/>
@@ -121,7 +125,7 @@ export default function Home() {
                   (data.projects != null) ? (
                      data.projects.map((ass) => (
                         <div key={ass.id}
-                           className={`p-2 px-6 w-full flex-wrap sm:flex group justify-between items-center max-w-4xl shadow-orangelg ml-0 bg-gray-50 border-x-4 
+                           className={`p-1 px-6 w-full flex-wrap sm:flex group justify-between items-center max-w-4xl shadow-orangelg ml-0 bg-gray-50 border-x-4 
                            hover:border-orange-400 font-medium rounded-lg transition-all duration-500 hover:ml-1`}
                            style={{}}
                         >
